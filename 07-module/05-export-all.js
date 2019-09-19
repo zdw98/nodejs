83130f830fb2aecf68fdd323e1adb542dd36518d
wangding <408542507@qq.com>
2019-09-19 10:49:37 +0800

    3

---
+++ 07-module/05-export-all.js
@@ -0,0 +1,10 @@
+  #!/usr/bin/node
+
+module.exports = {
+    pi: require('./02-export-var'),
+      circle: require('./02-export-function'),
+        Circle: require('./02-export-object')
+
+};
+
+console.dir(module);
